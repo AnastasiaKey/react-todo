3dd99a7aaf8bf4366c2b4e605a6201adad7126ab
Anastasia Klyucharova <anastasia@Anastasias-MBP.lan>
2023-06-07 22:22:22 -0400

    first fuction component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
