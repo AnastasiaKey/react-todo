59d8fe6dfeb67762a514e81ef35782cb23b99929
Anastasia Klyucharova <anastasia@Anastasias-MBP.lan>
2023-06-07 23:11:06 -0400

    todoList map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+const todoList = [
+  {
+    id:1,
+    title: "Complete assigment"
+  },
+  {
+    id:2,
+    title: "Run assigment"
+  },
+  {
+    id:3,
+    title: "Check assigment"
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map(function(item) {
+          return <li key={item.id}> 
+          <span>{item.title}</span> 
+                </li>
+        }
+        )}
+
+      </ul>
     </div>
   );
 }
