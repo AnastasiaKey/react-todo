7deaca0ec42ce6c88dc4e21a18da245088e01187
Anastasia Klyucharova <anastasia@Anastasias-MBP.lan>
2023-07-12 00:19:05 -0400

    Creat Custom Hook

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-function App() {
-
+const useSemiPersistentState = () => {
+  
   const [todoList, setTodoList] = React.useState(
     JSON.parse(localStorage.getItem('savedTodoList')) || []
     );
@@ -11,6 +11,13 @@ function App() {
   React.useEffect(() => {
     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
   }, [todoList]); 
+
+  return [todoList, setTodoList];
+
+};
+
+function App() {
+  const [todoList, setTodoList] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
