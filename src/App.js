8f4318ac8509de51cfd295a7748a800b9cd55647
Anastasia Klyucharova <anastasia@Anastasias-MBP.lan>
2023-08-23 23:16:37 -0400

    Setup Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
@@ -125,11 +131,20 @@ function App() {
   setTodoList(newremoveTodo);
 }
   return (
-    <>
-    <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo}/>
-      {isLoading?<p>Loading...</p>: <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>}
-    </>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div>
+             <h1>Todo List</h1>
+             <AddTodoForm onAddTodo={addTodo}/>
+              {isLoading?<p>Loading...</p>: <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>}
+          </div>
+        } />
+        <Route path="/new" element={<h1>New Todo List</h1>} />
+      </Routes>
+    </BrowserRouter>
+      
   );
 }
 
